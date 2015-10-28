9142db36c1c884aa54421febaf88e976b13b9f13
andip71 <andreasp@gmx.de>
2015-10-28 13:01:27 +0100

    cpufreq: add elementalx governor

--- drivers/cpufreq/cpufreq_governor.c
+++ drivers/cpufreq/cpufreq_governor.c
@@ -317,6 +317,9 @@ int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 		} else if (cdata->governor == GOV_ZZMOOVE) {
 			rc = cdata->init_zz(dbs_data, policy);
 		} else
+		if (cdata->governor == GOV_ELEMENTALX)
+			rc = cdata->init_ex(dbs_data, policy);
+		else
 			rc = cdata->init(dbs_data, policy);
 
 		if (rc) {
