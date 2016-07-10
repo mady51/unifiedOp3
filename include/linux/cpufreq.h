db85f99277ac9766ee144e210b144cace1bff551
Joe Maples <joe@frap129.org>
2016-07-10 17:26:44 +0000

    cpufreq: Add Chill cpu gov, squashed commit... cb2b873 chill: Allow any number >= 1 for boost count 4749892 chill: Reset boost count on policy->min b127052 chill: Decrease boost count alongside frequency 10b0d0c chill: Simplify boost increment logic aae1918 chill: I'm secretly retarded 4d5f0f9 chill: Reset boost count at max regardless of whether we've boosted c9a14a2 cpufreq: chill: Use native display_state instead of PowerSuspend 0231351 cpufreq: chill: Version 2.0 6f1be93 cpufreq: chill: Impliment down_threshold_suspended 41bfe3c cpufreq: chill: Replace sleep_depth with true load ignorance 6f5b528 cpufreq: chill: Guard against 0 sleep depth and optimize defaults 5933b1a cpufreq: chill: Go back to using Conservative's tunables 08522e9 cpufreq: chill: use GOV_CHILL macro 1f4c7a7 cpufreq: chill: Don't check for target frequency when boosting 7097ece cpufreq: chill: Major cleanup, move changes from governor.h to chill.h 47c5fa5 cpufreq: chill: Add boost option 3a3f782 cpufreq: chill: Add verion macros 1e858fb cpufreq: Add Chill cpu gov
    Signed-off-by: Joe Maples <joe@frap129.org>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -528,6 +528,9 @@ extern struct cpufreq_governor cpufreq_gov_zzmoove;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CLARITY)
 extern struct cpufreq_governor cpufreq_gov_clarity;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_clarity)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CHILL)
+extern struct cpufreq_governor cpufreq_gov_chill;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_chill)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
