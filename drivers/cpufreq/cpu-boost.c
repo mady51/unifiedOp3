03ead065935bbcf94fc81f121197d35bd80b997c
Francisco Franco <franciscofranco.1990@gmail.com>
2016-12-01 01:22:50 +0000

    drivers: cpu-boost: optimize policy update loop on input cpufreq boost
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -174,9 +174,10 @@ static void update_policy_online(void)
 		 * individually, saving at least one [down|up] write
 		 * and a [lock|unlock] irqrestore per pass
 		 */
-		pr_debug("Updating policy for CPU%d\n", i);
-		if (i == 0 || i == 4)
+		if ((i & 1) == 0) {
+			pr_debug("Updating policy for CPU%d\n", i);
 			cpufreq_update_policy(i);
+		}
 	}
 	put_online_cpus();
 }
