a85f166e9410c624c0b087114199d7ab151a1c18
franciscofranco <franciscofranco.1990@gmail.com>
2018-01-01 20:58:24 +0800

    wakeup: add some wakelocks from cheeseburger

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -34,6 +34,12 @@ static bool enable_timerfd_ws = true;
 module_param(enable_timerfd_ws, bool, 0644);
 static bool enable_netlink_ws = true;
 module_param(enable_netlink_ws, bool, 0644);
+static bool enable_netmgr_wl_ws = true;
+module_param(enable_netmgr_wl_ws, bool, 0644);
+static bool enable_wlan_pno_wl_ws = true;
+module_param(enable_wlan_pno_wl_ws, bool, 0644);
+static bool enable_wcnss_filter_lock_ws = true;
+module_param(enable_wcnss_filter_lock_ws, bool, 0644);
 
 /*
  * If set, the suspend/hibernate code will abort transitions to a sleep state
@@ -480,6 +486,12 @@ static bool wakeup_source_blocker(struct wakeup_source *ws)
 				!strncmp(ws->name, "wlan", wslen)) ||
 			(!enable_timerfd_ws &&
 				!strncmp(ws->name, "[timerfd]", wslen)) ||
+                        (!enable_netmgr_wl_ws &&
+                                !strncmp(ws->name, "netmgr_wl", wslen)) ||
+                        (!enable_wlan_pno_wl_ws &&
+                                !strncmp(ws->name, "wlan_pno_wl", wslen)) ||
+                        (!enable_wcnss_filter_lock_ws &&
+                                !strncmp(ws->name, "wcnss_filter_lock", wslen)) ||
 			(!enable_netlink_ws &&
 				!strncmp(ws->name, "NETLINK", wslen))) {
 			if (ws->active) {
