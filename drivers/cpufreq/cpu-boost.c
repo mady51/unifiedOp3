47bd5e1f0208a0d12eb670733707cd116b5377b4
root <root@mady.c.tranquil-bison-183411.internal>
2018-02-19 05:12:01 +0000

    removed unused thing

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -263,8 +263,6 @@ static void cpuboost_input_event(struct input_handle *handle,
 	if (now - last_input_time < (input_boost_ms * USEC_PER_MSEC))
 		return;
 
-	if (queuing_blocked(&cpu_boost_worker, &input_boost_work))
-		return;
 
 	queue_kthread_work(&cpu_boost_worker, &input_boost_work);
 	last_input_time = ktime_to_us(ktime_get());
