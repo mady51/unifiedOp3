77590a17a247fb49460b2da9d66b37845d3a01d0
Yaroslav Furman <yaro330@gmail.com>
2018-06-17 23:00:18 +0300

    cpu-boost: set default stune boost to 10
    No ramdisk - no candy.
    Gotta set everything up in kernel.
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -40,7 +40,7 @@ static unsigned int input_boost_ms = 40;
 module_param(input_boost_ms, uint, 0644);
 
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
-static int dynamic_stune_boost;
+static int dynamic_stune_boost = 10;
 module_param(dynamic_stune_boost, uint, 0644);
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
